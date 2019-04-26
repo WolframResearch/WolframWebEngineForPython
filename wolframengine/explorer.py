8d806d7e479754dfb3557fce1b70b74622e2addc
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-04-26 14:16:08 +0200

    path can now be something like //aaa/a///a/

--- wolframengine/explorer.py
+++ wolframengine/explorer.py
@@ -7,7 +7,7 @@ import os
 
 def get_wl_handler_path_from_folder(folder, path, index='index.m'):
 
-    absolute = os.path.join(folder, *path.split('/'))
+    absolute = os.path.join(folder, *filter(None, path.split('/')))
 
     if os.path.isdir(absolute):
         absolute = os.path.join(absolute, index)
