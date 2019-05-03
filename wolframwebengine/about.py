89af605127633769202fd5e89a9f7e12be3ec5dc
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-05-03 16:28:15 +0200

    removing about

--- wolframwebengine/about.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, print_function, unicode_literals
-
-__name__ = 'wolframwebengine'
-__description__ = 'A Python library with various tools to start a wolfram engine a server content.'
-__version__ = '0.0.1'
-__author__ = 'Wolfram Research'
-__author_email__ = 'riccardod@wolfram.com'
