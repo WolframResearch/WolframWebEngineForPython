59cb46c689bd537b822fea449e497a9e49567b51
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-05-03 16:28:43 +0200

    now is called wolframwebengine

--- wolframwebengine/__main__.py
+++ wolframwebengine/__main__.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from wolframengine.cli.commands.runserver import Command
+from wolframwebengine.cli.commands.runserver import Command
 
 if __name__ == '__main__':
     Command().main()
