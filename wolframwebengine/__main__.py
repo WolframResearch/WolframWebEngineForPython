d5ffbb0a68369e9422cae53baca8f398f785195f
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-05-03 16:28:20 +0200

    adding back main

---
+++ wolframwebengine/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from wolframengine.cli.commands.runserver import Command
+
+if __name__ == '__main__':
+    Command().main()
