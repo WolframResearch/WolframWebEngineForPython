d979f8f515246eb6bac4afa2c07c1a46b90b79d1
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-05-17 15:32:58 +0200

    moving some code, so that it can be tested in isolation

--- wolframwebengine/cli/commands/runserver.py
+++ wolframwebengine/cli/commands/runserver.py
@@ -2,18 +2,13 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-import os
-
 from aiohttp import web
 
 from wolframclient.cli.utils import SimpleCommand
 from wolframclient.evaluation import (WolframEvaluatorPool,
                                       WolframLanguageAsyncSession)
-from wolframclient.language import wl
 from wolframclient.utils.api import asyncio
-from wolframclient.utils.functional import last
-from wolframwebengine.explorer import get_wl_handler_path_from_folder
-from wolframwebengine.web import aiohttp_wl_view
+from wolframwebengine.server.app import create_session, create_view
 
 
 class Command(SimpleCommand):
@@ -31,9 +26,7 @@ class Command(SimpleCommand):
         parser.add_argument(
             '--domain', default='localhost', help='Insert the domain.')
         parser.add_argument(
-            '--kernel',
-            default=None,
-            help='Insert the kernel path.')
+            '--kernel', default=None, help='Insert the kernel path.')
         parser.add_argument(
             '--poolsize',
             default=1,
@@ -55,67 +48,11 @@ class Command(SimpleCommand):
             help='The file name to search for folder index.',
         )
 
-    def create_session(self, path, poolsize=1, **opts):
-        if poolsize <= 1:
-            return WolframLanguageAsyncSession(path, **opts)
-        return WolframEvaluatorPool(path, poolsize=poolsize, **opts)
-
-    EXTENSIONS = {
-        '.wxf': wl.Function(wl.Import(wl.Slot(), 'WXF')),
-        '.mx': wl.Function(wl.Import(wl.Slot(), 'MX')),
-        '.m': wl.Get,
-        '.wl': wl.Get,
-    }
-
-    def is_wl_code(self, path):
-        try:
-            return bool(self.get_wl_handler(path))
-        except KeyError:
-            return False
-
-    def get_wl_handler(self, path):
-        return self.EXTENSIONS[last(os.path.splitext(path)).lower()]
-
-    def create_view(self, session, path, cached, index):
-
-        path = os.path.abspath(os.path.expanduser(path))
-
-        @aiohttp_wl_view(session)
-        async def get_code(request, location=path):
-            expr = self.get_wl_handler(location)(location)
-            if cached:
-                return wl.Once(expr)
-            return expr
-
-        if os.path.isdir(path):
-
-            async def view(request):
-                loc = get_wl_handler_path_from_folder(
-                    path, request.path, index=index)
-
-                if not loc:
-                    return web.Response(body='Page not found', status=404)
-
-                if self.is_wl_code(loc):
-                    return await get_code(request, location=loc)
-                return web.FileResponse(loc)
-
-            return view
-
-        elif os.path.exists(path):
-            if not self.is_wl_code(path):
-                raise ValueError('%s must be one of the following formats: %s'
-                                 % (path, ', '.join(self.EXTENSIONS.keys())))
-            return get_code
-        else:
-            raise ValueError('%s is not an existing path on disk.' % path)
-
     def handle(self, domain, port, path, kernel, poolsize, lazy, cached,
                **opts):
 
-        session = self.create_session(
-            kernel, poolsize=poolsize, inputform_string_evaluation=False)
-        view = self.create_view(session, path, cached=cached, **opts)
+        session = create_session(kernel, poolsize=poolsize)
+        view = create_view(session, path, cached=cached, **opts)
 
         async def main():
 
---
+++ wolframwebengine/server/app.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+import os
+
+from aiohttp import web
+
+from wolframclient.evaluation import (WolframEvaluatorPool,
+                                      WolframLanguageAsyncSession)
+from wolframclient.language import wl
+from wolframclient.utils.functional import last
+from wolframwebengine.server.explorer import get_wl_handler_path_from_folder
+from wolframwebengine.web import aiohttp_wl_view
+
+EXTENSIONS = {
+    '.wxf': wl.Function(wl.Import(wl.Slot(), 'WXF')),
+    '.mx': wl.Function(wl.Import(wl.Slot(), 'MX')),
+    '.m': wl.Get,
+    '.wl': wl.Get,
+}
+
+
+def is_wl_code(path):
+    try:
+        return bool(get_wl_handler(path))
+    except KeyError:
+        return False
+
+
+def get_wl_handler(path):
+    return EXTENSIONS[last(os.path.splitext(path)).lower()]
+
+
+def create_session(path=None,
+                   poolsize=1,
+                   inputform_string_evaluation=False,
+                   **opts):
+    if poolsize <= 1:
+        return WolframLanguageAsyncSession(
+            path,
+            inputform_string_evaluation=inputform_string_evaluation,
+            **opts)
+    return WolframEvaluatorPool(
+        path,
+        poolsize=poolsize,
+        inputform_string_evaluation=inputform_string_evaluation,
+        **opts)
+
+
+def create_view(session, path, cached, index):
+
+    path = os.path.abspath(os.path.expanduser(path))
+
+    @aiohttp_wl_view(session)
+    async def get_code(request, location=path):
+        expr = get_wl_handler(location)(location)
+        if cached:
+            return wl.Once(expr)
+        return expr
+
+    if os.path.isdir(path):
+
+        async def view(request):
+            loc = get_wl_handler_path_from_folder(
+                path, request.path, index=index)
+
+            if not loc:
+                return web.Response(body='Page not found', status=404)
+
+            if is_wl_code(loc):
+                return await get_code(request, location=loc)
+            return web.FileResponse(loc)
+
+        return view
+
+    elif os.path.exists(path):
+        if not is_wl_code(path):
+            raise ValueError('%s must be one of the following formats: %s' %
+                             (path, ', '.join(EXTENSIONS.keys())))
+        return get_code
+    else:
+        raise ValueError('%s is not an existing path on disk.' % path)
