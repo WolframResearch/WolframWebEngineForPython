976c86c26b7c0656ca0f9b27b415f547f4353036
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-05-22 17:33:33 +0200

    sending a file

--- wolframwebengine/tests/application_django.py
+++ wolframwebengine/tests/application_django.py
@@ -3,10 +3,12 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from wolframclient.utils.tests import TestCase as BaseTestCase
-from django.urls import reverse
 from django.test import Client
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 from wolframwebengine.web.utils import auto_wait
 from wolframclient.utils.functional import first
+from wolframclient.utils.api import json
 
 
 class DjangoTestCase(BaseTestCase):
@@ -31,20 +33,32 @@ class DjangoTestCase(BaseTestCase):
 
     def test_django_app(self):
 
-        resp = self.client.get(reverse("home"))
+        resp = self.client.get("/")
 
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.content, b"hello from django")
 
-        resp = self.client.get(reverse("form"))
+        resp = self.client.get("/form")
 
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(first(resp["content-type"].split(";")), "text/html")
 
-        resp = self.client.get(reverse("api"))
+        resp = self.client.get("/api")
 
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(resp["content-type"], "application/json")
+
+        resp = self.client.get("/api?x=2")
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp["content-type"], "application/json")
+        self.assertEqual(json.loads(resp.content), {"x": "2"})
+
+        resp = self.client.post("/api", {"x": SimpleUploadedFile("foo.txt", b"foobar")})
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp["content-type"], "application/json")
+        self.assertEqual(json.loads(resp.content), {"x": "foobar"})
 
     def tearDown(self):
         if self.session.started:
