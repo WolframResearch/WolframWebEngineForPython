67fe0ceef7476cd178fc75cce38738b9bd9314aa
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-05-22 16:19:14 +0200

    adding a dummy placeholder for django implementation

---
+++ wolframwebengine/web/django.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+def generate_http_response(session, request, expression):
+    raise NotImplementedError(request, expression)
