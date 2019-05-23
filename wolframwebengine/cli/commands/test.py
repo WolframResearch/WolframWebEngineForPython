6d199be7b8c67affa97cc586269f971f26c421ba
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-05-23 16:22:29 +0200

    adding django dep

--- wolframwebengine/cli/commands/test.py
+++ wolframwebengine/cli/commands/test.py
@@ -3,10 +3,11 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from wolframclient.cli.commands.test import Command as TestCommand
+from wolframclient.utils.functional import iterate
 
 
 class Command(TestCommand):
 
     modules = ["wolframwebengine.tests"]
 
-    dependencies = [("django", "2.2.1")]
+    dependencies = tuple(iterate((("django", "2.2.1"),), TestCommand.dependencies))
