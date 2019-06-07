13fd9054b437ac7310f215960b0aaef291eecb7e
Dorian Birraux <dorianb@wolfram.com>
2019-06-07 16:55:20 +0200

    Create a function main()
    So that it can be call from an entrypoint

--- wolframwebengine/__main__.py
+++ wolframwebengine/__main__.py
@@ -4,5 +4,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from wolframwebengine.cli.commands.runserver import Command
 
-if __name__ == "__main__":
+def main():
     Command().main()
+
+if __name__ == "__main__":
+    main()
