e5751286cefc80c11a79e97a983749803640ae10
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-06-11 15:44:32 +0200

    refactor

--- wolframwebengine/__main__.py
+++ wolframwebengine/__main__.py
@@ -4,8 +4,10 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from wolframwebengine.cli.commands.runserver import Command
 
+
 def main():
     Command().main()
+
 
 if __name__ == "__main__":
     main()
