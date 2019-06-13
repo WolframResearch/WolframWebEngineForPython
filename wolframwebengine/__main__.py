e0973c11e2b5bbb3078ace1748a0ec940674b225
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-06-13 15:34:32 +0200

    remove encoding

--- wolframwebengine/__main__.py
+++ wolframwebengine/__main__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
-
 from __future__ import absolute_import, print_function, unicode_literals
 
 from wolframwebengine.cli.commands.runserver import Command
 
+
 def main():
     Command().main()
+
 
 if __name__ == "__main__":
     main()
