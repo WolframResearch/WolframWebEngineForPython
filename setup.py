9afc3b619e3dc0dddb8e5d9704546a3cc5a989d3
Dorian Birraux <dorianb@wolfram.com>
2019-06-13 12:28:32 +0200

    Add missing comma

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     classifiers = [
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
