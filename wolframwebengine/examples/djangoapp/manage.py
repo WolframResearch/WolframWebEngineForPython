09451623d530cc6ad05de70288e8ed1dded65784
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-10-22 14:10:24 -0500

    adding a manage file for the example app

---
+++ wolframwebengine/examples/djangoapp/manage.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+import sys
+
+
+def main():
+    try:
+        from django.core.management import execute_from_command_line
+        from django.conf import settings
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    settings.configure(
+        ROOT_URLCONF="wolframwebengine.examples.djangoapp.urls", ALLOWED_HOSTS="*", DEBUG=True
+    )
+
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
